cbc05f47eaa395b3062c81fb3ed46952769dad44
ccramcc <caiyaramsey@gmail.com>
2026-04-06 08:59:15 -0400

    enemy moves toward player! update player object to be real player once merged

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -1,18 +1,29 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class EnemyMovement : MonoBehaviour
 {
-    //[Header("References")]
+    //i am mainly copying this code from unity forums
+    //i will try to explain what the code is supposed to do
+    [Header("References")] 
+    [SerializeField] private GameObject player;
+    [SerializeField] private float enemySpeed = 5.0f; //feel free to change this, i dont remember how to refactor this so that it is in correlation with the variables of the other script >.>
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        StartCoroutine(EnemyMovementFunction());
     }
+
+    IEnumerator EnemyMovementFunction()
+    {
+        //delays the function for half a second then follows player
+        yield return new WaitForSeconds(0.5f);
+        transform.position = Vector3.MoveTowards(transform.position, player.transform.position, enemySpeed * Time.deltaTime);
+        Debug.Log("Targeting Player");
+        //function should target the player, then begin to move towards the target
+        //debug shows function is working
+    }
+
+    //damage to player done through player script, not this one
 }

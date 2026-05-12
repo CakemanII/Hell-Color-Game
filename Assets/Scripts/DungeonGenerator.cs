f343ec9a2d149148ed0f56e692c39869197cccc1
Kat-Paw <katpawling@gmail.com>
2026-05-12 09:03:29 -0400

    code??

---
+++ Assets/Scripts/DungeonGenerator.cs
@@ -0,0 +1,65 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DungeonGenerator : MonoBehaviour
+{
+   /* public static DungeonGenerator Instance { get; private set; }
+
+    [SerializeField] private GameObject entrance;
+
+    [SerializeField] private List<GameObject> rooms;
+
+    [SerializeField] private List<GameObject> specialRooms;
+
+    [SerializeField] private List<GameObject> alternateEntrances;
+
+    [SerializeField] private List<GameObject> hallways;
+
+    [SerializeField] private GameObject door;
+
+
+    [SerializeField] private int noOfRooms = 10;
+    [SerializeField] private LayerMask roomsLayermask;
+
+    private List<DungeonPart> generatedRooms;
+    private bool isGenerated = false;
+
+    private void Awake()
+    {
+
+    }
+
+    private void Start()
+    {
+
+    }
+
+    public void StartGeneration()
+    {
+        StartGenerationServerRpc();
+
+    }
+
+    [ServerRpc(RequireOwnershp = false)]
+    private void StartGenerationServerRpc()
+    {
+        Generate
+        GenerateAlternateEntrances();
+        FillEmptyEntrances();
+        isGenerated = true;
+    }
+
+    priavte void Generate()
+    {
+        for i = 0; isGenerated < noOfRooms - alternateEntrances.Count; i++)
+        {
+            if (generatedRooms.Count < 1)
+            {
+                GameObject generatedRooms = AsyncInstantiateOperation(entrance, transform.position, Transform.rotation);
+            }
+        }
+    }
+
+    */
+
+}
---
+++ Assets/Scripts/DungeonPart.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class DungeonPart : MonoBehaviour
+{
+/*
+    public enum DungeonPartType
+    {
+        Room,
+        Hallway
+    }
+
+    [SerializeField] private LayerMask roomsLayermask;
+
+    [SerializeField] private DungeonPartType dungeonPartType;
+
+    [SerializeField] private GameObject fillerWall;
+
+    public List<Transform> entrypoints;
+
+    public new Collider collider;
+
+    public bool HasAvailableEntrypoint(out Transform entrypoint)
+    {
+        Transform resultingEntry = null;
+        bool result = false;
+
+        int totalRetries = 100;
+        int retryIndex = 0;
+
+        if (entrypoints.Count = 1)
+        {
+            Transform entry = entrypoints[0];
+            if (entry.TryGetComponent<EntryPoint>(out entry Point res))
+            {
+                if (result IsOccupied())
+                {
+
+                }
+            }
+        }
+    }
+*/
+}

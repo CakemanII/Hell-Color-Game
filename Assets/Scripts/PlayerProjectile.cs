a77b7ef86133e3596deb6eef2ab24ac683b98e48
charlie <bellagbomb@gmail.com>
2026-05-12 10:32:43 -0400

    reload

--- Assets/Scripts/PlayerProjectile.cs
+++ Assets/Scripts/PlayerProjectile.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Runtime.CompilerServices;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class PlayerProjectile : MonoBehaviour
 {
@@ -9,6 +11,10 @@ public class PlayerProjectile : MonoBehaviour
     public float bulletSpeed;
     public float fireRate, bulletDamage;
     public bool isAuto;
+
+    [Header("Ammo Variables")]
+    public int bullets;
+    public TextMeshProUGUI ammoText;
 
     [Header("Initial Setup")]
     public Transform bulletSpawnTransform;
@@ -18,21 +24,28 @@ public class PlayerProjectile : MonoBehaviour
 
     public void Update()
     {
+        ammoText.text = bullets.ToString();
+
         if(timer > 0)
         {
             timer -= Time.deltaTime / fireRate;
         }
 
+        if (bullets == 0 && Input.GetKeyDown(KeyCode.R))
+        {
+            bullets = 10;
+        }
+
         if (isAuto)
         {
-            if (Input.GetButton("Fire1") && timer <= 0)
+            if (Input.GetButton("Fire1") && timer <= 0 && bullets > 0)
             {
                 Shoot();
             }
         }
         else
         {
-            if(Input.GetButtonDown("Fire1") && timer <= 0)
+            if(Input.GetButtonDown("Fire1") && timer <= 0 && bullets > 0)
             {
                 Shoot();
             }
@@ -41,6 +54,8 @@ public class PlayerProjectile : MonoBehaviour
 
     void Shoot()
     {
+        bullets--;
+
         GameObject bullet = Instantiate(bulletPrefab, bulletSpawnTransform.position, Quaternion.identity, GameObject.FindGameObjectWithTag("WorldObjectHolder").transform);
         bullet.GetComponent<Rigidbody>().AddForce(bulletSpawnTransform.forward * bulletSpeed, ForceMode.Impulse);
         bullet.GetComponent<Bullet>().damage = bulletDamage;

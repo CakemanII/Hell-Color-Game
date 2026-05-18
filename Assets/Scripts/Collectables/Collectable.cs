702822c9f97a286a7e47de8870602c39019dcc88
CakemanII <tyler.m.jundt@icloud.com>
2026-05-18 12:24:34 -0400

    .

--- Assets/Scripts/Collectables/Collectable.cs
+++ Assets/Scripts/Collectables/Collectable.cs
@@ -2,13 +2,17 @@ using UnityEngine;
 
 public class Collectable : MonoBehaviour
 {
+    private int quantity;
+    public int Quantity => quantity;
+
+    public void Init(int quantity)
+    {
+        this.quantity = quantity;
+    }
+
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
-        {
-            // Implement collectable logic here, e.g., add to inventory, play sound, etc.
-            Debug.Log("Collectable picked up!");
-            Destroy(gameObject); // Remove the collectable from the scene
-        }
+
     }
 }
